c2afb10721579c7fc0eb3dcc5d83aec861398181
Graham Reeves <graham@grahamreeves.com>
2019-04-11 15:13:25 +0100

    Added mem debug

--- VideoTest/bootup.js
+++ VideoTest/bootup.js
@@ -4,6 +4,9 @@ Pop.Include = function(Filename)
 	return Pop.CompileAndRun( Source, Filename );
 }
 
+
+let Debug = Pop.Debug;
+
 Pop.Include('../Common/PopEngine.js');
 Pop.Include('../Common/PopShaderCache.js');
 Pop.Include('../Common/TFrameCounter.js');
@@ -12,3 +15,32 @@ Pop.Include('PopBrowser.js');
 Pop.Include('mp4.js');
 Pop.Include('Video.js');
 Pop.Include('VideoTest.js');
+
+
+
+
+
+let MemCheckLoop = async function()
+{
+	while(true)
+	{
+		try
+		{
+			await Pop.Yield(1000);
+			//Pop.GarbageCollect();
+			let ImageHeapSize = (Pop.GetImageHeapSize() / 1024 / 1024).toFixed(2) + "mb";
+			let ImageHeapCount = Pop.GetImageHeapCount();
+			let GeneralHeapSize = (Pop.GetHeapSize() / 1024 / 1024).toFixed(2) + "mb";
+			let GeneralHeapCount = Pop.GetHeapCount();
+			Pop.Debug("Memory: ImageHeapSize="+ImageHeapSize+" x" + ImageHeapCount + " GeneralHeapSize=" + GeneralHeapSize + " x"+GeneralHeapCount);
+
+			let GeneralHeapObjects = Pop.GetHeapObjects();
+			Pop.Debug(JSON.stringify(GeneralHeapObjects));
+		}
+		catch(e)
+		{
+			Pop.Debug("Loop Error: " + e );
+		}
+	}
+}
+MemCheckLoop();
