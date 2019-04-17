0a14bf9d0ebb9e37fe7ce449a490e6a89cd31f24
Graham Reeves <graham@grahamreeves.com>
2019-04-17 16:05:46 +0100

    Better memory report loop

--- CameraTest/bootup.js
+++ CameraTest/bootup.js
@@ -8,3 +8,36 @@ Pop.Include('../Common/PopEngine.js');
 Pop.Include('../Common/PopShaderCache.js');
 Pop.Include('../Common/TFrameCounter.js');
 Pop.Include('CameraTest.js');
+
+
+
+let MemCheckLoop = async function()
+{
+	while(true)
+	{
+		try
+		{
+			await Pop.Yield(500);
+			Pop.GarbageCollect();
+			let Debug = "Memory: ";
+			
+			let ImageHeapSize = (Pop.GetImageHeapSize() / 1024 / 1024).toFixed(2) + "mb";
+			let ImageHeapCount = Pop.GetImageHeapCount();
+			Debug += " ImageHeapSize="+ImageHeapSize+" x" + ImageHeapCount;
+			
+			let GeneralHeapSize = (Pop.GetHeapSize() / 1024 / 1024).toFixed(2) + "mb";
+			let GeneralHeapCount = Pop.GetHeapCount();
+			Debug += " GeneralHeapSize="+GeneralHeapSize+" x" + GeneralHeapCount;
+			
+			Debug += JSON.stringify(Pop.GetHeapObjects());
+			Pop.Debug(Debug);
+		}
+		catch(e)
+		{
+			Debug("Loop Error: " + e );
+		}
+	}
+}
+MemCheckLoop();
+
+
