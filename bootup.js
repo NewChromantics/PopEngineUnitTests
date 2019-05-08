6d57203cdebd456406deb78b5dc72a4f8445909f
Graham Reeves <graham@grahamreeves.com>
2019-05-08 12:45:11 +0100

    Added colour changing loop to the basic unit test to show engine iteration is working

--- bootup.js
+++ bootup.js
@@ -2,10 +2,38 @@
 
 Pop.Debug("Hello World");
 
+let Colour = 0;
+
 function OnRender(RenderTarget)
 {
-	RenderTarget.ClearColour(255,0,0);
+	let Red = Colour & 0xff;
+	let Green = (Colour >> 8) & 0xff;
+	let Blue = (Colour >> 16) & 0xff;
+	RenderTarget.ClearColour(Red/256, Green/256, Blue/256);
 }
 
 let Window = new Pop.Opengl.Window("A Pop Engine window!");
 Window.OnRender = OnRender;
+
+async function Loop()
+{
+	while(true)
+	{
+		await Pop.Yield(10);
+		Colour++;
+		Pop.Debug(Colour);
+		let Red = Colour & 0xff;
+		let Green = (Colour >> 8) & 0xff;
+		let Blue = (Colour >> 16) & 0xff;
+		Pop.Debug( Red, Green, Blue );
+	}
+}
+
+function OnError(Error)
+{
+	Pop.Debug("Error: " + Error);
+	//	blue
+	Colour = 0xff << 16;
+}
+
+Loop().catch( OnError );
