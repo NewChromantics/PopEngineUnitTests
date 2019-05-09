5e0f3b96cdf4cc27e32f64e3fa3fdda64d879f6e
Graham Reeves <graham@grahamreeves.com>
2019-05-09 17:01:03 +0100

    Put framecounter in basic test to check update & render speed

--- bootup.js
+++ bootup.js
@@ -1,6 +1,15 @@
 //	replace this with a proper mini unit test/api example set of scripts
-
 Pop.Debug("Hello World");
+
+Pop.Include = function(Filename)
+{
+	const Source = Pop.LoadFileAsString(Filename);
+	return Pop.CompileAndRun( Source, Filename );
+}
+Pop.Include("Common/TFrameCounter.js");
+
+let UpdateCounter = new TFrameCounter("Update");
+let RenderCounter = new TFrameCounter("Render");
 
 let Colour = 0;
 
@@ -10,7 +19,9 @@ function OnRender(RenderTarget)
 	let Green = (Colour >> 8) & 0xff;
 	let Blue = (Colour >> 16) & 0xff;
 	RenderTarget.ClearColour(Red/256, Green/256, Blue/256);
+	RenderCounter.Add(1);
 }
+
 
 let Window = new Pop.Opengl.Window("A Pop Engine window!");
 Window.OnRender = OnRender;
@@ -19,13 +30,9 @@ async function Loop()
 {
 	while(true)
 	{
-		await Pop.Yield(10);
+		await Pop.Yield( 1000/250 );
 		Colour++;
-		Pop.Debug(Colour);
-		let Red = Colour & 0xff;
-		let Green = (Colour >> 8) & 0xff;
-		let Blue = (Colour >> 16) & 0xff;
-		Pop.Debug( Red, Green, Blue );
+		UpdateCounter.Add(1);
 	}
 }
 
