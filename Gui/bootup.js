49472195f397ae0dc7087caaef84ffed2f86a721
Graham Reeves <graham@grahamreeves.com>
2019-05-22 14:19:06 +0100

    Added textbox to demo

--- Gui/bootup.js
+++ Gui/bootup.js
@@ -1,9 +1,14 @@
 
 let Window = new Pop.Gui.Window("Platform Native Window!");
 let Slider = new Pop.Gui.Slider( Window, [0,0,9999,80] );
+let Label = new Pop.Gui.TextBox( Window, [0,80,9999,30] );
+Label.SetValue("Hello #");
 Slider.SetMinMax( 0, 100 );
 Slider.SetValue( 33 );
-Slider.OnChanged = Pop.Debug;
+Slider.OnChanged = function(Value)
+{
+	Label.SetValue("Hello " + Value);
+}
 
 let Slider2 = new Pop.Gui.Slider( Window, [0,50,9999,20] );
 Slider2.SetMinMax( 0, 100 );
