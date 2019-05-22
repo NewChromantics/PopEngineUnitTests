0d7b563cf3ef54ee81eb9b1169af6a24cadc575b
Graham Reeves <graham@grahamreeves.com>
2019-05-22 13:44:21 +0100

    Slider testing

--- Gui/bootup.js
+++ Gui/bootup.js
@@ -1,7 +1,13 @@
 
 let Window = new Pop.Gui.Window("Platform Native Window!");
-let Slider = new Pop.Gui.Slider( Window, [0,0,100,20] );
+let Slider = new Pop.Gui.Slider( Window, [0,0,9999,80] );
 Slider.SetMinMax( 0, 100 );
 Slider.SetValue( 33 );
 Slider.OnChanged = Pop.Debug;
 
+let Slider2 = new Pop.Gui.Slider( Window, [0,50,9999,20] );
+Slider2.SetMinMax( 0, 100 );
+Slider2.OnChanged = function(Value)
+{
+	Slider.SetValue(Value);
+}
