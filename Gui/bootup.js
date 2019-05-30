28557293d7bb99e19da29aaa9c4ff66029bcee3d
Graham Reeves <graham@grahamreeves.com>
2019-05-30 17:56:24 +0100

    Added tickbox to gui example

--- Gui/bootup.js
+++ Gui/bootup.js
@@ -1,7 +1,7 @@
 
 let Window = new Pop.Gui.Window("Platform Native Window!");
 let Slider = new Pop.Gui.Slider( Window, [0,0,9999,80] );
-let Label = new Pop.Gui.TextBox( Window, [0,80,9999,30] );
+let Label = new Pop.Gui.Label( Window, [0,80,9999,30] );
 Label.SetValue("Hello #");
 Slider.SetMinMax( 0, 100 );
 Slider.SetValue( 33 );
@@ -16,3 +16,12 @@ Slider2.OnChanged = function(Value)
 {
 	Slider.SetValue(Value);
 }
+
+let TickBox = new Pop.Gui.TickBox( Window, [0,120,9999,80] );
+let TickLabel = new Pop.Gui.Label( Window, [0,200,9999,30] );
+TickBox.SetValue(false);
+TickBox.OnChanged = function(Value)
+{
+	TickLabel.SetValue("Tickbox = " + Value);
+}
+
