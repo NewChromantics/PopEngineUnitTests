b87243f8a9666fa416b2975bf3fbd7f1334bf5c8
Graham Reeves <graham@grahamreeves.com>
2019-06-02 17:34:17 +0100

    extracting h264 properly

--- H264Encode/bootup.js
+++ H264Encode/bootup.js
@@ -13,12 +13,24 @@ async function Run(Filename)
 	while ( true )
 	{
 		const Packet = await Encoder.GetNextPacket();
+		//Pop.Debug("Packet",typeof Packet);
 		if ( !Packet )
-			break;
-	
-		const Frame = Decoder.PushData(Packet);
+			continue;
+		const ExtractPlanes = false;
+		const Frames = await Decoder.Decode(Packet,ExtractPlanes);
+		Pop.Debug(JSON.stringify(Frames));
+		if ( Frames.length == 0 )
+			continue;
+
+		Pop.Debug("Frames",Frames);
+		Pop.Debug(Frames.length);
+		const Frame = Frames[0].Planes[0];
 		if ( Frame )
+		{
+			Pop.Debug("Output frame",Frame.GetFormat());
 			Output = Frame;
+		}
+		
 	}
 }
 Run('cat.jpeg').then(Pop.Debug).catch(Pop.Debug);
