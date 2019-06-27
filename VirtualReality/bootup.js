d2716aca136eaeb36e4cc75b83a25cfeb468a985
Graham Reeves <graham@grahamreeves.com>
2019-06-27 23:28:38 +0100

    openhmd vr

--- VirtualReality/bootup.js
+++ VirtualReality/bootup.js
@@ -1,12 +1,14 @@
 //	replace this with a proper mini unit test/api example set of scripts
 Pop.Debug("Virtual Reality Test");
 
+Pop.Debug( JSON.stringify( Pop.Openhmd.EnumDevices() ) );
 
 //	find devices
 //	attach to device
 //	get updates
 //	render
-let Hmd = new Pop.Openvr.Hmd("Device Name");
+//let Hmd = new Pop.Openvr.Hmd("Device Name");
+let Hmd = new Pop.Openhmd.Hmd("OpenHmd_0");
 
 Hmd.OnRender = function(RenderTarget,Camera)
 {
@@ -17,3 +19,25 @@ Hmd.OnRender = function(RenderTarget,Camera)
 	else
 		RenderTarget.ClearColour( 0,0,1 );
 }
+
+
+function HmdDk1(Hmd)
+{
+	//	create fullscreen window in the right screen
+	let Screens = Pop.EnumScreens();
+	Pop.Debug( JSON.stringify(Screens) );
+	this.Window = new Pop.Opengl.Window()
+
+	this.OnRender = function(RenderTarget)
+	{
+		let LeftMatrix = Hmd.GetEyeMatrix("Left");
+		let RightMatrix = Hmd.GetEyeMatrix("Right");
+		//	render to left
+		//	render to right
+		//	render both!
+		Hmd.OnRender( RenderTarget, "Left" );
+	}
+	
+	this.Window.OnRender = this.OnRender;
+}
+Hmd.Handler = new HmdDk1(Hmd);
