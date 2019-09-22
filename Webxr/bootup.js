570fabb9f424a11962845bd6891dee0b561e9825
Graham Reeves <graham@grahamreeves.com>
2019-09-22 20:56:32 +0100

    Webxr rendering

--- Webxr/bootup.js
+++ Webxr/bootup.js
@@ -3,16 +3,26 @@
 function SetupXrDevice(Device)
 {
 	Pop.Debug("New device",Device);
+	Device.OnRender = RenderEye;
+}
+
+function RenderEye(RenderTarget,Camera)
+{
+	if ( Camera.Name == 'Left' )
+		RenderTarget.ClearColour( 0,0.5,1 );
+	else if ( Camera.Name == 'Right' )
+		RenderTarget.ClearColour( 1,0,0 );
+	else
+		RenderTarget.ClearColour( 0,0,1 );
 }
 
 
 function RenderWindow(RenderTarget)
 {
-	RenderTarget.ClearColour( 0,0,1 );
+	RenderTarget.ClearColour( 0,1,0 );
 }
 
-//	todo: attrib
-//	xrCompatible: true
+//	todo: attrib	{xrCompatible: true}, but working with mozilla's emulator...
 let Window = new Pop.Opengl.Window("xr");
 Window.OnRender = RenderWindow;
 
