eba431180e2caf75d4a22b714f33d711702fa3d5
Graham Reeves <graham@grahamreeves.com>
2020-01-22 17:41:07 +0000

    Added colour to new params window

--- ParamsWindow/bootup.js
+++ ParamsWindow/bootup.js
@@ -15,8 +15,7 @@ Params.Degrees = 0;
 Params.WholeDegrees = 0;
 Params.OneToThree = 2;
 Params.BouncingNumber = 0;
-//Params.TheColour = [1,1,1];
-Params.TheColour = [1,1,1];
+Params.TheColour = [1,0.5,0.1];
 Params.TheString = 'hello';
 Params.TheButton = 'Click me';
 
@@ -34,7 +33,7 @@ ParamsWindow.AddParam('Degrees',-180,180);
 ParamsWindow.AddParam('WholeDegrees',-180,180,Math.floor);
 ParamsWindow.AddParam('OneToThree',1,3,Math.floor);
 ParamsWindow.AddParam('BouncingNumber',-1,1);
-//ParamsWindow.AddParam('TheColour','Colour');
+ParamsWindow.AddParam('TheColour','Colour');
 ParamsWindow.AddParam('TheString');
 ParamsWindow.AddParam('TheButton','Button');
 
