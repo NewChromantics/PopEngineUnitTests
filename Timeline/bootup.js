e8544dd758db2fc4c2d8dc321fdf72ebbf3a1512
Graham Reeves <graham@grahamreeves.com>
2020-03-13 18:00:48 +0000

    Starting timeline implementation

---
+++ Timeline/bootup.js
@@ -0,0 +1,28 @@
+
+
+const WindowRect = [100,100,400,400];
+const Window = new Pop.Gui.Window("Timeline",WindowRect,false);
+const LabelRect = [0,0,WindowRect[2],WindowRect[3]/3];
+const Label = new Pop.Gui.Label(Window,LabelRect);
+Label.SetValue("Hello #");
+
+const ImageMapTop = LabelRect[1] + LabelRect[3];
+const ImageMapHeight = WindowRect[3] - ImageMapTop;
+const ImageMapRect = [0,ImageMapTop,WindowRect[2],ImageMapHeight];
+const ImageMap = new Pop.Gui.ImageMap(Window,ImageMapRect);
+
+async function ImageMapLoop()
+{
+	let Pixels = new Pop.Image('cat.jpeg');
+	let CursorMap = [ [0,1],[2,3] ];	//	2x2
+	ImageMap.SetImage(Pixels);
+	ImageMap.SetCursorMap(CursorMap,['pointer','help','not-allowed','wait']);
+
+	while (ImageMap)
+	{
+		const Event = await ImageMap.WaitForMouseEvent();
+
+		Pop.Debug("Image map event",JSON.stringify(Event));
+	}
+}
+ImageMapLoop().then(Pop.Debug).catch(Pop.Debug);
