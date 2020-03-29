e2bfe71f0e32c30ab864b246d6447c3c5ac61353
Graham Reeves <graham@grahamreeves.com>
2020-03-29 22:12:12 +0100

    New engine bootup app which waits for a Pop app to be drag & dropped onto window

---
+++ bootup.js
@@ -0,0 +1,40 @@
+
+//	todo: load this layout from a xib!
+const Window = new Pop.Gui.Window("Pop Engine");
+
+//const LabelRect = Window.GetScreenRect();
+const LabelRect = [0,0,400,200];
+
+const Label = new Pop.Gui.Label( Window, LabelRect );
+Label.SetValue('Drag an app folder to this window to boot');
+
+async function RunNextApp(Filename)
+{
+	//	relaunch this app with a new path
+	let ExeName = Pop.GetExeDirectory() + 'PopEngine';
+	let Arguments = [Filename];
+	const Process = new Pop.ShellExecute(ExeName,Arguments);
+	const ExitCode = await Process.WaitForExit();
+	Pop.Debug(`${ExeName} exited with ${ExitCode}`);
+}
+
+async function LoadProjectLoop()
+{
+	while ( true )
+	{
+		const DroppedFilenames = await Window.WaitForDragDrop();
+		const RunFilename = DroppedFilenames[0];
+		
+		try
+		{
+			await RunNextApp(RunFilename);
+		}
+		catch(e)
+		{
+			Pop.Debug(`Error running ${RunFilename}; ${e}`);
+		}
+		
+		await Pop.Yield(1000);
+	}
+}
+LoadProjectLoop().then().catch();
