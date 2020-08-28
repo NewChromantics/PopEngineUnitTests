5e24c4a72bdb5475efbbe7fd5b9de3964eea3d94
thomasSDK <tsdkelly@protonmail.com>
2020-08-28 12:14:32 +0100

    simplify sokol test

--- Sokol/bootup.js
+++ Sokol/bootup.js
@@ -1,15 +1,12 @@
 Pop.Debug("Hello World")
 
 Pop.Debug(Pop.Sokol)
-const x = new Pop.Sokol.Sokol();
 
-// const window = new Pop.Gui.Window('XibName');
-// const RenderView = new Pop.Gui.RenderView('NameOfViewInXib');
-// const Render= new Pop.Sokol.Test(RenderView);
+const Window = new Pop.Gui.Window("Sokol Test");
 
 try
 {
-	 x.Test()
+	const Sokol = new Pop.Sokol.Sokol(Window)
 }
 catch(e)
 {
