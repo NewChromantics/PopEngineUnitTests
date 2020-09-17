6ff3774ca8b205a2a9a577e00496fdc4ab6f223d
Graham Reeves <graham@grahamreeves.com>
2020-09-17 19:48:06 +0100

    first pass at sokol commands

--- Sokol/bootup.js
+++ Sokol/bootup.js
@@ -4,11 +4,28 @@ Pop.Debug(Pop.Sokol)
 
 const Window = new Pop.Gui.Window("Sokol Test");
 
-try
+const Sokol = new Pop.Sokol.Context(Window, "GLView");
+
+let Counter = 0;
+function GetRenderCommands()
 {
-	const Sokol = new Pop.Sokol.Context(Window, "GLView");
+	const Commands = [];
+	const Blue = (Counter % 60)/60;
+	Commands.push(['Clear',1,0,Blue]);
+	return Commands;
 }
-catch(e)
+
+async function RenderLoop()
 {
-	Pop.Debug(e)
+	while (Sokol)
+	{
+		//	submit frame for next paint
+		const Commands = GetRenderCommands();
+		Pop.Debug(`Render ${Counter}`);
+		await Sokol.Render(Commands);
+		Counter++;
+	}
 }
+RenderLoop();
+
+
