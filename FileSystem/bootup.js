7e554a3609eb6958fa1a65ed67f0d417af7f1827
thomasSDK <tsdkelly@protonmail.com>
2020-10-01 19:28:09 +0100

    added test for zip and WriteToFile

--- FileSystem/bootup.js
+++ FileSystem/bootup.js
@@ -2,3 +2,18 @@ const Devices = Pop.GetExternalDrives();
 Pop.Debug(Devices);
 
 Pop.Debug(Pop.GetTempDirectory())
+
+const DirectoryName = "Test";
+const Filename = "Test.json";
+
+const ZipName = `Test_${Pop.GetTimeNowMs()}.zip`
+const Archive = new Pop.Zip.Archive( ZipName );
+
+let TestJSON = {hello: "World"};
+
+let json = JSON.stringify( TestJSON );
+
+Pop.WriteToFile( `${DirectoryName}/${Filename}`, JSON.stringify( TestJSON ) );
+//await Archive.AddFile( `${DirectoryName}/${Filename}`, `${DirectoryName}/${Filename}` );
+
+Archive.Close()
