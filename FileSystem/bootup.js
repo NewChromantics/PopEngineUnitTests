e9249a2f3fc6176a96c4c7dcfe67e3f89111ceac
thomasSDK <tsdkelly@protonmail.com>
2020-10-06 12:25:15 +0100

    async wrapper for archive code

--- FileSystem/bootup.js
+++ FileSystem/bootup.js
@@ -1,19 +1,24 @@
-const Devices = Pop.GetExternalDrives();
-Pop.Debug(Devices);
+async function test()
+{
+	const Devices = Pop.GetExternalDrives();
+	Pop.Debug(Devices);
+	
+	Pop.Debug(Pop.GetTempDirectory())
 
-Pop.Debug(Pop.GetTempDirectory())
+	const DirectoryName = "Test";
+	const Filename = "Test.json";
 
-const DirectoryName = "Test";
-const Filename = "Test.json";
+	const ZipName = `Test_${Pop.GetTimeNowMs()}.zip`
+	let Archive = new Pop.Zip.Archive( ZipName );
 
-const ZipName = `Test_${Pop.GetTimeNowMs()}.zip`
-const Archive = new Pop.Zip.Archive( ZipName );
+	let TestJSON = { hello: "World" };
 
-let TestJSON = {hello: "World"};
+	let json = JSON.stringify( TestJSON );
 
-let json = JSON.stringify( TestJSON );
+	Pop.WriteToFile( `${DirectoryName}/${Filename}`, json );
+	await Archive.AddFile( `${DirectoryName}/${Filename}`, `${DirectoryName}/${Filename}` );
 
-Pop.WriteToFile( `${DirectoryName}/${Filename}`, JSON.stringify( TestJSON ) );
-//await Archive.AddFile( `${DirectoryName}/${Filename}`, `${DirectoryName}/${Filename}` );
+	Archive.Close();
+}
 
-Archive.Close()
+test();
